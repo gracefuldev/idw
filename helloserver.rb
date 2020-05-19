7277327385b4791d811a2f7185dd9a4df5778837
Avdi Grimm <avdi@avdi.codes>
2020-05-19 00:41:36 +0000

    Make helloserver.rb bind to 0.0.0.0
    For better conflictiness

--- helloserver.rb
+++ helloserver.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 require "webrick"
-server = WEBrick::HTTPServer.new(Port: 8080)
+server = WEBrick::HTTPServer.new(BindAddress: "0.0.0.0", Port: 8080)
 server.mount_proc("/") { |req, res| res.body = "Hello, world!" }
 trap("INT") { server.shutdown }
 server.start
