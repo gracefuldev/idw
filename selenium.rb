708799330736a1107cd43eb53e3e75f46542d782
Avdi Grimm <avdi@avdi.codes>
2020-05-19 00:42:15 +0000

    Fix selenium API invocation

--- selenium.rb
+++ selenium.rb
@@ -1,6 +1,3 @@
 require "selenium-webdriver"
-driver = Selenium::WebDriver.for(
-  :chrome,
-  options: { browser: :remote, url: "http://host.docker.internal:9515" },
-)
+driver = Selenium::WebDriver.for(:remote, url: "http://host.docker.internal:9515")
 driver.get "https://avdi.codes"
