7651d0c6860f88aa7cd4a7c475c64dac34a21a2d
Jessica Kerr <jessitron@gmail.com>
2020-08-10 20:22:26 -0500

    aliases

--- bin/environment.sh
+++ bin/environment.sh
@@ -1,3 +1,5 @@
 export PATH=/workspaces/idw/bin:/workspaces/idw/vendor/perf-tools/bin:$PATH
 . /usr/local/share/chruby/chruby.sh
 chruby 2.7
+alias ll='ls -l'
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
