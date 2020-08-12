e264cf4a39984b29f4b60fc7faef5e715b4958bf
Avdi Grimm <avdi@avdi.codes>
2020-08-11 21:53:39 -0400

    Add network access to blurp.sh

--- blurp.sh
+++ blurp.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 echo "I am blurp ($$)"
+echo "I will definitely not use the network!"
+read -p "Press any key..."
 echo "blurp" > /tmp/blurp$$.tmp
+curl -H "Accept: text/plain" https://icanhazdadjoke.com/ 
+echo ""
 date > ~/.lastblurp
