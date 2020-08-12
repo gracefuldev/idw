a530bebd6cd9c38684659b0c3e3c4990eae10e24
Jessica Kerr <jessitron@gmail.com>
2020-08-12 23:25:46 +0000

    Typo in blurp

--- blurp.sh
+++ blurp.sh
@@ -3,7 +3,7 @@ echo "I am blurp ($$)"
 echo "I will definitely not use the network! or write any files."
 read -p "Press enter..."
 echo "blurp" > /tmp/blurp$$.tmp
-echo "But I will tell you a joke!\n"
+echo "But I will tell you a joke!"
 curl -s -H "Accept: text/plain" https://icanhazdadjoke.com/  | /usr/games/cowsay
 echo ""
 date > ~/.lastblurp
